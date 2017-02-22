53fbe4faac43e22af52a5101bb8c34e7392bc97a
Varadharajan <srinathsmn@gmail.com>
2017-02-22 13:08:58 +0530

    5 - Dummy tests for repository

---
+++ booking_site/test_repository.py
@@ -0,0 +1,14 @@
+from unittest import TestCase
+from unittest.mock import patch
+
+class TestRepository(TestCase):
+	def setUp(self):
+		# Ensure we have a clean database
+		# db_manager.truncate_all_tables()
+		pass
+
+	def test_should_persist_ticket_to_database(self):
+		# ticket = Ticket("varadha", 1, "bus 1")
+		# repository.persist(ticket)
+		# self.assertEquals(ticket, repository.getTicket("varadha"))
+		pass
