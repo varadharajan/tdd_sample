3e70672f04f5cdbf8b0cddcd67d1d0119484cc2f
Varadharajan <srinathsmn@gmail.com>
2017-02-22 12:09:08 +0530

    3 - Enhance tests

--- booking_site/test_provider.py
+++ booking_site/test_provider.py
@@ -4,7 +4,15 @@ from unittest.mock import patch
 from provider import *
 from service import Ticket
 
+import requests_mock
+
 class TestProvider(TestCase):
+
+
 	def test_should_publish_booked_ticket(self):
 		ticket = Ticket("varadha", 1, "Bus 1")
-		self.assertTrue(publish(ticket))
+		
+		with requests_mock.Mocker() as m:
+			m.post('http://awesome-mq.com/', json={"status": "ok"})
+			self.assertTrue(publish(ticket))
+			self.assertTrue(m.called)
