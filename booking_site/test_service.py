7231be5ebb4e9ec467f24bee08c0d494905a6984
Varadharajan <srinathsmn@gmail.com>
2017-02-22 11:33:52 +0530

    2 - Added tests for validating Tickets

--- booking_site/test_service.py
+++ booking_site/test_service.py
@@ -2,7 +2,7 @@ from unittest import TestCase
 from unittest.mock import patch
 
 
-from service import Ticket, Service
+from service import *
 
 class TestService(TestCase):
 
@@ -12,9 +12,14 @@ class TestService(TestCase):
 		repositoryMock.return_value = True
 		providerMock.return_value = True
 
-		ticket = Ticket("varadha", "S1", "Bus")
+		ticket = Ticket("varadha", 1, "Bus")
 
 		self.assertTrue(Service.book(ticket))
 
 		repositoryMock.assert_called_with(ticket)
 		providerMock.assert_called_with(ticket)
+
+	def test_should_raise_exception_if_ticket_details_are_invalid(self):
+		ticket = Ticket("varadha", -100, "Bus")
+		with self.assertRaises(InvalidTicketException):
+			Service.book(ticket)
